141cdfc51b00bf6dde6d6ed0aa58c936bdaa8c98
adityakrshnn <adityakrshnn@gmail.com>
2022-02-02 21:14:27 -0700

    fix: removed dotenc

--- app/handler.ts
+++ app/handler.ts
@@ -1,11 +1,4 @@
 import { Handler } from 'aws-lambda';
-// import dotenv from 'dotenv';
-// import path from 'path';
-// const dotenvPath = path.join(__dirname, '../', `config/.env`);
-// dotenv.config({
-//   path: dotenvPath,
-// });
-
 import { Render } from './controller/render';
 
 export const process: Handler = (event: any) => {
